646cce8d42b51457a49cd923cc5c50a59a2ce168
hardik kacha <hardik@fafadiatech.com>
2021-10-25 15:38:58 +0530

    added crud operations

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -4,35 +4,38 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import * as Yup from 'yup';
 import { Formik } from 'formik';
-import { FiTrash2 } from "react-icons/fi";
+import { FiCheck, FiX } from "react-icons/fi";
 import { Row, Col, Form, Button, Container } from 'react-bootstrap';
 
 // Import components
 import ALL_DAYS from '../constants';
+import ReadOnlyRows from '../components/ReadOnlyRows';
+import EditableRow from '../components/EditableRow';
 
 const commonErrorMsg = "This field is required";
 
 const validationSchema = Yup.object().shape({
-    dayType: Yup.string().required(commonErrorMsg),
-    daysOfWeek: Yup.string().required(commonErrorMsg),
+    day_type: Yup.string().required(commonErrorMsg),
+    day_of_week: Yup.string().required(commonErrorMsg),
     every: Yup.string().required(commonErrorMsg),
-    startTime: Yup.string().required(commonErrorMsg),
-    endTime: Yup.string().required(commonErrorMsg)
+    start_time: Yup.string().required(commonErrorMsg),
+    end_time: Yup.string().required(commonErrorMsg)
 });
 
 const getAllDays = ALL_DAYS.map((item, index) => {
     return (<option value={item} key={index}>{item}</option>)
-})
+});
 
 const Home = () => {
 
     const [availabilites, setAvailabilites] = useState([])
     const [isFormVisible, setIsFormVisible] = useState(false)
+    const [editContactId, seteditContactId] = useState(null)
 
     const getAllAvailabilites = async (id) => {
         await axios({
             method: "GET",
-            url: `/availabilities/${id}`,
+            url: `/availabilities/carer/${id}`,
         }).then(res => {
             setAvailabilites(res.data)
         })
@@ -41,15 +44,58 @@ const Home = () => {
     const handleDelete = async (id) => {
         await axios({
             method: "DELETE",
-            url: `/delete-availability/${id}`,
+            url: `/availabilities/delete/${id}`,
         }).then(res => {
-            setAvailabilites(availabilites.filter((el => el._id !== id)))
+            setAvailabilites(availabilites.filter((el => el.id !== id)))
         });
+    }
+
+    const handleClickEditRow = (e, id) => {
+        e.preventDefault();
+        seteditContactId(id)
+    }
+    
+    const onSubmit = async (values, {setSubmitting, resetForm}) => {
+        setSubmitting(true)
+        await axios({
+            method: "POST",
+            data: values,
+            url: '/availabilities/create',
+        }).then(res => {
+            const data = res.data;
+            setTimeout(() => {
+                setAvailabilites({...availabilites, data})
+                resetForm();
+                setSubmitting(false)
+                setIsFormVisible(false)
+            }, 500);
+        })
+    }
+
+    const onEditSubmit = async (values, {setSubmitting, resetForm}) => {
+        setSubmitting(true)
+        await axios({
+            method: "PUT",
+            data: values,
+            url: `/availabilities/update/${values.id}`,
+        }).then(res => {
+            const data = res.data;
+            // const getIndex = availabilites.findIndex((item) => item.id)
+            // console.log(getIndex)
+            // availabilites[getIndex] = data
+
+            setTimeout(() => {
+                resetForm();
+                setSubmitting(false)
+                seteditContactId(null)
+                setAvailabilites(prevData => ({...prevData, availabilites:data}))
+            }, 500);
+        })
     }
 
     useEffect(() => {
         getAllAvailabilites(1);
-    }, [availabilites])
+    }, [availabilites.length])
 
     return (
         <React.Fragment>
@@ -58,113 +104,66 @@ const Home = () => {
 
                 <Row>
                     <Col lg={7}>
-                        {
-                            availabilites.map((item, index) => {
-                                return(
-                                    <Row key={index}>
-                                        <Col>
-                                            <div className="item-data text-capitalize">{item.dayType}</div>
-                                        </Col>
-                                        <Col>
-                                            <div className="item-data text-capitalize">{item.daysOfWeek}</div>
-                                        </Col>
-                                        <Col>
-                                            <div className="item-data text-capitalize">{item.every}</div>
-                                        </Col>
-                                        <Col>
-                                            <div className="item-data">{item.startTime}</div>
-                                        </Col>
-                                        <Col>
-                                            <div className="item-data">{item.endTime}</div>
-                                        </Col>
-                                        <Col>
-                                            <div className="trash" onClick={() => handleDelete(item._id)}>
-                                                <FiTrash2 className="text-danger" />
-                                            </div>
-                                        </Col>
-                                    </Row>
-                                )
-                            })
-                        }
-                        {/* <table className="table">
-                            <tbody>
+                        { availabilites.length > 0 &&
+                            <React.Fragment>
+                                <Row>
+                                    <Col><span className="fw-bold">Day Type</span></Col>
+                                    <Col><span className="fw-bold">Day Of Week</span></Col>
+                                    <Col><span className="fw-bold">Every</span></Col>
+                                    <Col><span className="fw-bold">Start Time</span></Col>
+                                    <Col><span className="fw-bold">End Time</span></Col>
+                                    <Col><span className="fw-bold">Action</span></Col>
+                                </Row>
                                 {
-                                    availabilites.map((item, index) => {
-                                        return(
-                                            <tr key={index}>
-                                                <td className="text-capitalize">{item.dayType}</td>
-                                                <td>{item.daysOfWeek}</td>
-                                                <td>{item.every}</td>
-                                                <td>{item.startTime}</td>
-                                                <td>{item.endTime}</td>
-                                                <td><FiTrash2 className="text-danger" /></td>
-                                            </tr>
-                                        )
-                                    })
+                                    availabilites.map((item, index) => (
+                                        <React.Fragment key={index}>
+                                            {editContactId === item.id ?
+                                                <EditableRow item={item} handleClickEditRow={handleClickEditRow} onSubmit={onEditSubmit} validationSchema={validationSchema} />
+                                            :
+                                                <ReadOnlyRows item={item} handleClickEditRow={handleClickEditRow} handleDelete={handleDelete} />
+                                            }
+                                        </React.Fragment>
+                                    ))
                                 }
-                            </tbody>
-                        </table> */}
+                            </React.Fragment>
+                        }
                     </Col>
                 </Row>
 
                 <Row>
-                    <Col lg={7} className="mt-3">
+                    <Col lg={7}>
                         {isFormVisible ?
                             <Formik
-                                initialValues={{ carer: 1, dayType: "", daysOfWeek: "", every: "", startTime: "", endTime: "" }}
+                                initialValues={{ carer_id: 1, day_type: "", day_of_week: "", every: "", start_time: "", end_time: "" }}
                                 validationSchema={validationSchema}
-                                onSubmit={async (values, {setSubmitting, resetForm}) => {
-                                    setSubmitting(true)
-
-                                    await axios({
-                                        method: "POST",
-                                        data: values,
-                                        url: '/create',
-                                    }).then(res => {
-                                        setTimeout(() => {
-                                            resetForm();
-                                            setSubmitting(false)
-                                            setIsFormVisible(false)
-                                        }, 500);
-                                    })
-                                }}
+                                onSubmit={onSubmit}
                             >
                             {( {values, errors, touched, handleChange, handleBlur, handleSubmit, isSubmitting }) => (
                                 <Form onSubmit={handleSubmit}>
-                                    <Row className="d-none">
-                                        <Col lg={12}>
-                                            <Form.Group className="mb-3">
-                                                <Form.Label>Carer:</Form.Label>
-                                                <Form.Control type="text" value={values.carer} name="carer" readOnly />
-                                            </Form.Group>
-                                        </Col>
-                                    </Row>
                                     <Row>
                                         <Col>
-                                            <Form.Group className="mb-3">
-                                                <Form.Label>Day Type:</Form.Label>
-                                                <Form.Select value={values.dayType} onChange={handleChange} onBlur={handleBlur} name="dayType">
+                                            <Form.Group className="my-2">
+                                                <Form.Control type="text" className="d-none" value={values.carer_id} name="carer_id" readOnly />
+                                                <Form.Select value={values.day_type} onChange={handleChange} onBlur={handleBlur} name="day_type">
                                                     <option>Select</option>
                                                     <option value="daily">Daily</option>
                                                     <option value="specific-day">On Specific Day</option>
                                                 </Form.Select>
-                                                {touched.dayType && errors.dayType ? (<div className="error-message">{errors.dayType}</div>): null}
+                                                {touched.day_type && errors.day_type ? (<div className="error-message">{errors.day_type}</div>): null}
                                             </Form.Group>
                                         </Col>
                                         <Col>
-                                            <Form.Group className="mb-3">
-                                                <Form.Label>Days Of Week:</Form.Label>
-                                                <Form.Select value={values.daysOfWeek} onChange={handleChange} onBlur={handleBlur} name="daysOfWeek">
+                                            <Form.Group className="my-2">
+                                                <Form.Select value={values.day_of_week} onChange={handleChange} onBlur={handleBlur} name="day_of_week">
                                                     <option>Select</option>
                                                     <option value="All">All</option>
                                                     {getAllDays}
                                                 </Form.Select>
-                                                {touched.daysOfWeek && errors.daysOfWeek ? (<div className="error-message">{errors.daysOfWeek}</div>): null}
+                                                {touched.day_of_week && errors.day_of_week ? (<div className="error-message">{errors.day_of_week}</div>): null}
                                             </Form.Group>
                                         </Col>
                                         <Col>
-                                            <Form.Group className="mb-3">
-                                                <Form.Label>Every:</Form.Label>
+                                            <Form.Group className="my-2">
                                                 <Form.Select value={values.every} onChange={handleChange} onBlur={handleBlur} name="every" >
                                                     <option>Select</option>
                                                     <option value="All">All</option>
@@ -177,30 +176,41 @@ const Home = () => {
                                             </Form.Group>
                                         </Col>
                                         <Col>
-                                            <Form.Group className="mb-3">
-                                                <Form.Label>Start Time:</Form.Label>
-                                                <Form.Control type="text" name="startTime" placeholder="0:00 AM" onChange={handleChange} onBlur={handleBlur} value={values.startTime} />
-                                                {touched.startTime && errors.startTime ? (<div className="error-message">{errors.startTime}</div>): null}
+                                            <Form.Group className="my-2">
+                                                <Form.Control type="text" name="start_time" placeholder="0:00 AM" onChange={handleChange} onBlur={handleBlur} value={values.start_time} />
+                                                {touched.start_time && errors.start_time ? (<div className="error-message">{errors.start_time}</div>): null}
                                             </Form.Group>
                                         </Col>
                                         <Col>
-                                            <Form.Group className="mb-3">
-                                                <Form.Label>End Time:</Form.Label>
-                                                <Form.Control type="text" name="endTime" placeholder="0:00 AM" onChange={handleChange} onBlur={handleBlur} value={values.endTime} />
-                                                {touched.endTime && errors.endTime ? (<div className="error-message">{errors.endTime}</div>): null}
+                                            <Form.Group className="my-2">
+                                                <Form.Control type="text" name="end_time" placeholder="0:00 AM" onChange={handleChange} onBlur={handleBlur} value={values.end_time} />
+                                                {touched.end_time && errors.end_time ? (<div className="error-message">{errors.end_time}</div>): null}
                                             </Form.Group>
                                         </Col>
-                                    </Row>
-                                    <Row>
                                         <Col>
-                                            <Button type="submit" disabled={isSubmitting} className="btn btn-success btn-sm">Submit</Button>
+                                            <Form.Group>
+                                                <ul className="m-0 list-inline">
+                                                    <li className="list-inline-item">
+                                                        <Button type="submit" variant="link" disabled={isSubmitting} size="sm" style={{padding: '0px'}}>
+                                                            <div className="btn-icon" style={{padding: '0px'}}>
+                                                                <FiCheck className="text-success" />
+                                                            </div>
+                                                        </Button>
+                                                    </li>
+                                                    <li className="list-inline-item">
+                                                        <div className="trash">
+                                                            <FiX className="text-danger" />
+                                                        </div>
+                                                    </li>
+                                                </ul>
+                                            </Form.Group>
                                         </Col>
                                     </Row>
                                 </Form>
                             )}
                             </Formik>
                         :
-                            <p className="text-primary fw-bold mt-3" onClick={() => setIsFormVisible(true)}>Add more +</p>
+                            <Button variant="link" size="sm" className="text-primary fw-bold mt-3" style={{paddingLeft:'0px'}} onClick={() => setIsFormVisible(true)}>Add more +</Button>
                         }
                     </Col>
                 </Row>
